28304bb4e89dc5919af73594bfab0d741b466cb1
fabiana <fabianamfranco@outlook.com>
2021-08-09 15:38:21 -0300

    com aulas a mais

--- Bloco 4/4.3/exercícios.js
+++ Bloco 4/4.3/exercícios.js
@@ -1,32 +1,81 @@
 // ATIVIDADES:
 // 1- Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n . 
-// Por exemplo: Copiar n = 5
+// Por exemplo: n = 5
 //*****
 //*****
 //*****
 //*****
 //*****
-// Para n, numero de linhas = n; numero de asteriscos = n
 
+// Para n, numero de linhas = n; numero de asteriscos = n
 //let n = 5
 //let cadaLinha = '*' * n
 //let numLinhas = cadaLinha * n
 //let impressao = cadaLinha * numLinhas
+//______________________________________________________________________________________________________________________//
+
+// let n = 5;
+// let cadaLinha = '*';
+// cadaLinha = cadaLinha * n; 
+// for(let x = 0; x < n; x += 1){
+//   if (x < n){
+//     console.log(cadaLinha);
+//   }
+// }
+
+// let cadaLinha = '*****'
+// console.log(cadaLinha)
+// console.log(cadaLinha)
+// console.log(cadaLinha)
+// console.log(cadaLinha)
+// console.log(cadaLinha)
+
+// _____________________________________________________________________________________________________________________//
+// 2- Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base. 
+// Por exemplo: n = 5
+// *
+// **
+// ***
+// ****
+// *****
+
+// let n = 5;
+// let cadaLinha = '*';
+// for (let i = 0; i < n; i += 1){
+//   console.log(cadaLinha);
+//   for(let x = 0; x < n; x += 1){
+//     if (x < n){
+//       console.log(cadaLinha);
+//     }
+//   }
+// }
+
+//________________________________________________________________________________________________________________________//
+// 3- Agora inverta o lado do triângulo. 
+// Por exemplo: n = 5
+//     *
+//    **
+//   ***
+//  ****
+// *****
+// Atenção! Note que esse exercício é bem mais complexo que o anterior! Não basta, aqui, imprimir somente asteriscos. Você precisará de uma lógica para imprimir espaços também.
+
+//________________________________________________________________________________________________________________________//
+// 4- Depois, faça uma pirâmide com n asteriscos de base:
+// Por exemplo: n = 5
+//   *
+//  ***
+// *****
 
 
-//let n = 5;
-//let cadaLinha = "*";
-//for(let i = 0; i < n; i += 1) {
-    //cadaLinha = cadaLinha * i;
-//}    
-//for(let x = 0; x < n; x += 1){
-    //console.log(cadaLinha) * x;
-//}
+//________________________________________________________________________________________________________________________//
+// 5- Faça uma pirâmide com n asteriscos de base que seja vazia no meio. Assuma que o valor de n será sempre ímpar:
+// Por último, façamos com que a variável seja incrementada com o valor correspondente a cada loop;
+// n = 7
+//    *
+//   * *
+//  *   *
+// *******
 
 
-let cadaLinha = '*****'
-console.log(cadaLinha)
-console.log(cadaLinha)
-console.log(cadaLinha)
-console.log(cadaLinha)
-console.log(cadaLinha)
+//________________________________________________________________________________________________________________________//
